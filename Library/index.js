edf515ac478ddfa57051fa7aa383e7d28da7c8f3
Glebanuk <glebbong12387@gmail.com>
2023-08-25 19:31:32 +0300

    feat: add register modal

--- Library/index.js
+++ Library/index.js
@@ -1,4 +1,18 @@
 console.log('Задание выполнено на 100% по всем пунктам требований\nоцениваю работу в 50 баллов');
+// pop-up links-------------------------------------------------------------------------------
+const profileBtn = document.querySelector(".ico-profile__button");
+const profileMenu = document.querySelector(".auth__container");
+
+const registerBtn = document.querySelectorAll(".register__btn");
+const registerContainer = document.querySelector(".pop-up__body");
+const registerCloseBtn = document.querySelector(".close-register-btn");
+const registerContent = document.querySelector(".pop-up__content");
+const registerUsername =document.getElementById('first-name-input');
+const registerLastname =document.getElementById('last-name-input');
+const registerEmail =document.getElementById('email-input-register');
+const registerPassword = document.getElementById('password-register');
+const registerForm = document.querySelector(".register-form");
+
 
 // burger------------------------------------------------------------------------------------------
 (function () {
@@ -55,7 +69,7 @@ console.log('Задание выполнено на 100% по всем пунк�
 
 }())
 // slider -----------------------------------------------------------------------------------------
-
+function slider() {
 const slider = document.querySelector(".slider");
 const indicators = document.querySelectorAll(".indicator");
 
@@ -78,12 +92,15 @@ indicators.forEach((currentIndicator, index) => {
         currentIndicator.classList.add("active");
     }
 });
-// pop-up authorithation profile------------------------------------
+}
 
-const profileBtn = document.querySelector(".ico-profile__button");
-const profileMenu = document.querySelector(".auth__container");
+slider();
 
-const handleClick = (event) => {
+// POP-UP --------------------------------------------------------------------------------------
+//  authorithation profile--------------
+function setupProfileMenu() {
+
+    const handleClick = (event) => {
     profileMenu.classList.toggle('open')
 }
 
@@ -94,10 +111,99 @@ document.addEventListener('click', (event) =>{
 });
 
 profileBtn.addEventListener('click', handleClick);
+    }
+
+setupProfileMenu();
+// register modal window----------------------------------------
+function registerMenu(){
+    
+//                 ---open\close modal---
+registerBtn.forEach(btn =>{
+    btn.addEventListener('click', () => {
+        registerContainer.classList.toggle('open-register');
+        profileMenu.classList.toggle('open')
+    })
+})
+    registerCloseBtn.addEventListener('click', () =>{
+        registerContainer.classList.toggle('open-register');
+        registerForm.reset();
+    })
+
+    registerContainer.addEventListener('click', (event) =>{
+        if(!registerContent.contains(event.target) ){
+            registerContainer.classList.toggle('open-register');
+            registerForm.reset();
+        }
+       });
+//                ----inputs behavior------------
+
+registerForm.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    validateInputs();
+})
+
+const setError = (element, message) =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add("success");
+    inputControl.classList.remove('error');
+};
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+  const validateInputs = () =>{
+    const usernameValue = registerUsername.value.trim();
+    const lastnameValue =  registerLastname.value.trim();
+    const emailValue = registerEmail.value.trim();
+    const passwordValue = registerPassword.value.trim();
+
+    if(usernameValue === ''){
+        setError(registerUsername, "First name is required");
+    }else{
+        setSuccess(registerUsername);
+    }
+
+    if(lastnameValue === ''){
+        setError(registerLastname, "Last name is required");
+    }else{
+        setSuccess(registerLastname);
+    }
+
+    if(emailValue === '') {
+        setError(registerEmail, "Email is required");
+    }else if (!isValidEmail(emailValue)) {
+        setError(registerEmail, "Enter a valid email");
+    }else {
+        setSuccess(registerEmail);
+    }
+
+    if(passwordValue === ''){
+        setError(registerPassword, 'Password is required');
+    }else if (passwordValue.length < 8){
+        setError(registerPassword, 'At least 8 symbols');
+    }else{
+        setSuccess(registerPassword)
+    }
 
 
+  };
 
 
+}
+
+ registerMenu();
 
 
 
