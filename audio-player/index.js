336187519e966e3c1b1d8e24c28a225e7493058e
Glebanuk <glebbong12387@gmail.com>
2023-09-21 19:30:50 +0300

    feat: add foo set music/ foo duration music time

--- audio-player/index.js
+++ audio-player/index.js
@@ -4,7 +4,7 @@ const music = document.getElementById("audio");
 
 const disk =document.querySelector(".disk");
 const artist = document.querySelector(".Artist");
-const name = document.querySelector(".song-name");
+const songName = document.querySelector(".song-name");
 
 const seekBar = document.querySelector(".seek-bar");
 const current = document.querySelector(".current-time");
@@ -20,8 +20,56 @@ const volumeBtn = document.querySelector(".volume-btn");
 
 
 playBtn.addEventListener("click", () => {
+    // setMusic();
     playBtn.classList.toggle("pause");
     disk.classList.toggle("rotate");
 })
 
 
+
+function setMusic (i) {
+    seekBar.value = 0;
+    let song = songs[i];
+
+    currentSong = i;
+    music.src = song.path;
+
+    songName.innerHTML = song.name;
+    artist.innerHTML = song.artist;
+    disk.style.background = `url(${song["img-cover"]})`;
+
+    current.innerHTML = "00:00";
+
+    music.addEventListener("loadedmetadata", function() {
+        seekBar.max = music.duration;
+        duration.innerHTML = formatTime(music.duration);
+        
+    })
+    // setTimeout(() => {
+    //     seekBar.max = music.duration;
+    //     console.log(music.duration)
+
+    // }, 300)
+
+}
+
+// formating time
+
+setMusic(0);
+
+const formatTime = (time)  =>{
+    let min = Math.floor(time / 60);
+
+    if(min < 10){
+        min = `0${min}`;
+
+    }
+
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`
+    }
+    
+    return `${min} : ${sec}`
+
+}
