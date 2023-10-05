fb1e4b05e7b90a0dc1968482b87d50f34a7e3c32
Glebanuk <glebbong12387@gmail.com>
2023-10-05 17:04:07 +0300

    feat: add start modal

--- random-game/index.js
+++ random-game/index.js
@@ -2,9 +2,24 @@ const WALL = document.querySelector(".wall");
 const HOLE = document.querySelector(".hole");
 const UFO = document.querySelector(".UFO");
 const scorePoint = document.querySelector(".scoreboard");
+const startWindow = document.querySelector(".game-start");
 
 let score = 1;
+let isAnimationRun = false
 
+
+// start game---------------------
+
+document.addEventListener("keydown", (e) =>{
+
+    if(e.code == "Space" && !isAnimationRun){
+        startWindow.classList.add("close-start");
+        HOLE.style.animationPlayState = "running";
+        WALL.style.animationPlayState = "running";
+        isAnimationRun = true;
+        requestAnimationFrame(fallingUfo);
+    }
+})
 
 WALL.addEventListener('animationiteration', () =>{
     holePosition()
@@ -17,24 +32,22 @@ function holePosition() {
     HOLE.style.top = -(Math.trunc(Math.random()*400) + 100) + "px" ;
 }
 
+
 function fallingUfo() {
     const curPosition = parseInt(window.getComputedStyle(UFO).getPropertyValue("top"));
     const changePosition = (curPosition + 2) + "px";
     UFO.style.top = changePosition;
     requestAnimationFrame(fallingUfo);
 }
-requestAnimationFrame(fallingUfo);
-
 
 function moveUfoUp() {
     const curPosition = parseInt(window.getComputedStyle(UFO).top);
-    UFO.style.top = (curPosition - 50) + "px";
+    UFO.style.top = (curPosition - 30) + "px";
 }
-
 
 function moveUfoDown() {
     const curPosition = parseInt(window.getComputedStyle(UFO).getPropertyValue("top"));
-    UFO.style.top = (curPosition + 50) + "px";
+    UFO.style.top = (curPosition + 30) + "px";
 }
 
 document.addEventListener("keydown", (e) => {
